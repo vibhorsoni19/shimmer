220fa2d5a802b11da51d2089133dc8e3a5f2b1a3
vibhorsoni19 <vibhor10soni1999@gmail.com>
2024-11-23 12:12:47 +0530

    infinit scroll

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,22 +3,40 @@ import Card from "./Card";
 import Shimmer from "./shimmer";
 
 const Body = () => {
-  const [memes, setMemes] = useState(null);
-
+  const [memes, setMemes] = useState([]);
+  const [showShimmer, setShowShimmer] = useState(false);
   useEffect(() => {
     getMemes();
-  }, []);
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  },[]);
+
+  const handleScroll = () => {
+    //scroll y=  how much i have scrolled
+    //innerHeight = height of the window(visible section)
+    //document.body.scrollHeight =  total height of web page
+    if (window.scrollY + window.innerHeight >= document.body.scrollHeight) {
+      console.log("fetch more data");
+      getMemes();
+    }
+  };
 
   const getMemes = async () => {
+    setShowShimmer(true);
     const getData = await fetch("https://meme-api.com/gimme/50");
     const jsonData = await getData.json();
-    setMemes(jsonData.memes);
+    setShowShimmer(false);
+    setMemes((memes) => [...memes, ...jsonData.memes]);
   };
 
   return (
     <div className="cardContainer">
-      {memes ? memes.map((meme, index) => <Card key={index} data={meme} />): <Shimmer />}
-     
+      {memes.map((meme, index) => (
+        <Card key={index} data={meme} />
+      ))}
+
+      {showShimmer && <Shimmer />}
     </div>
   );
 };
